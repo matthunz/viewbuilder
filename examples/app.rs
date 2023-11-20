e98f1fd3e2fc4c46463944836996e5b3e040ba9e
Matt Hunzinger <matthunz2@gmail.com>
2023-11-20 00:00:44 -0500

    Try out bump allocation

--- examples/app.rs
+++ examples/app.rs
@@ -1,35 +1,11 @@
-use viewbuilder::Object;
+use bumpalo::Bump;
+use viewbuilder::{Tree, View};
 
-enum Message {
-    Increment,
-    Decrement,
+fn app(bump: &Bump) -> impl View {
+    &**bump.alloc("")
 }
 
-#[derive(Default)]
-struct Counter {
-    value: i32,
-}
-
-impl Object for Counter {
-    type Message = Message;
-
-    fn update(&mut self, msg: &Self::Message) {
-        match msg {
-            Message::Increment => self.value += 1,
-            Message::Decrement => self.value -= 1,
-        }
-        dbg!(self.value);
-    }
-}
-
-#[tokio::main]
-async fn main() {
-    let a = viewbuilder::spawn(Counter::default());
-   
-    a.listen(|msg | {
-        dbg!("msg");
-    });
-
-    a.update(Message::Increment);
-
+fn main() {
+    let mut tree = Tree::new(app);
+    tree.view();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,163 +1,64 @@
-use dyn_hash::DynHash;
-use lazy_static::lazy_static;
-use slotmap::{DefaultKey, SlotMap};
-use std::{
-    any::Any,
-    collections::HashMap,
-    marker::PhantomData,
-    sync::{
-        atomic::{AtomicU64, Ordering},
-        Arc,
-    },
-};
-use tokio::sync::mpsc;
+use bumpalo::Bump;
+use std::mem;
 
-pub struct ObjectRef<T> {
-    id: u64,
-    _marker: PhantomData<T>,
+pub trait View<'a> {
+    type Element;
+
+    fn build(&'a self) -> Self::Element;
+
+    fn rebuild(&'a self, element: &mut Self::Element);
 }
 
-impl<T> Clone for ObjectRef<T> {
-    fn clone(&self) -> Self {
-        *self
-    }
+pub struct Tree<V> {
+    component: fn(&Bump) -> V,
+    frame_a: Bump,
+    frame_b: Bump,
+    is_frame_a: bool,
 }
 
-
-impl<T> Copy for ObjectRef<T> {
-
-} 
-
-impl<T> ObjectRef<T> {
-    pub fn update(self, msg: T::Message)
+impl<V> Tree<V> {
+    pub fn new<'a>(component: fn(&'a Bump) -> V) -> Self
     where
-        T: Object,
+        V: View<'a> + 'a,
     {
-        let rt = Runtime::current();
-        rt.tx
-            .send(Message::Update {
-                id: self.id,
-                msg: Arc::new(msg),
-            })
-            .unwrap();
-    }
-
-    pub fn listen(self, mut f: impl FnMut(&T::Message) + Send + 'static)
-    where
-        T: Object,
-    {
-        let rt = Runtime::current();
-        rt.tx.send(Message::Listen {
-            id: self.id,
-            f: Box::new(move |msg| f(msg.downcast_ref().unwrap())),
-        }).unwrap();
-    }
-}
-
-pub fn spawn<T>(object: T) -> ObjectRef<T>
-where
-    T: Object + 'static,
-{
-    let rt = Runtime::current();
-    let id = rt.next_id.fetch_add(1, Ordering::SeqCst);
-    rt.tx
-        .send(Message::Insert {
-            id,
-            object: Box::new(object),
-        })
-        .unwrap();
-
-    ObjectRef {
-        id,
-        _marker: PhantomData,
-    }
-}
-
-pub trait Object: Send {
-    type Message: Send + Sync + 'static;
-
-    fn update(&mut self, msg: &Self::Message);
-}
-
-pub trait AnyObject: Send {
-    fn any_update(&mut self, msg: Arc<dyn Any>);
-}
-
-impl<T> AnyObject for T
-where
-    T: Object,
-{
-    fn any_update(&mut self, msg: Arc<dyn Any>) {
-        Object::update(self, msg.downcast_ref().unwrap())
-    }
-}
-
-pub enum Message {
-    Insert {
-        id: u64,
-        object: Box<dyn AnyObject>,
-    },
-    Update {
-        id: u64,
-        msg: Arc<dyn Any + Send + Sync>,
-    },
-    Listen {
-        id: u64,
-        f: Box<dyn FnMut(Arc<dyn Any + Send + Sync>) + Send>,
-    },
-}
-
-#[derive(Clone)]
-pub struct Runtime {
-    tx: mpsc::UnboundedSender<Message>,
-    next_id: Arc<AtomicU64>,
-}
-
-impl Runtime {
-    fn new() -> Self {
-        let (tx, mut rx) = mpsc::unbounded_channel();
-
-        tokio::spawn(async move {
-            let mut objects = HashMap::new();
-            let mut listeners: HashMap<
-                u64,
-                Vec<Box<dyn FnMut(Arc<dyn Any + Send + Sync>) + Send>>,
-            > = HashMap::new();
-
-            while let Some(msg) = rx.recv().await {
-                match msg {
-                    Message::Insert { id, object } => {
-                        objects.insert(id, object);
-                    }
-                    Message::Update { id, msg } => {
-                        objects.get_mut(&id).unwrap().any_update(msg.clone());
-                        if let Some(listeners) = listeners.get_mut(&id) {
-                            for listener in listeners {
-                                listener(msg.clone());
-                            }
-                        }
-                    }
-                    Message::Listen { id, f } => {
-                        if let Some(listeners) = listeners.get_mut(&id) {
-                            listeners.push(f);
-                        } else {
-                            listeners.insert(id, vec![f]);
-                        }
-                    }
-                }
-            }
-        });
-
+        let component = unsafe {
+             mem::transmute(component)
+        };
         Self {
-            tx,
-            next_id: Arc::new(AtomicU64::new(0)),
+            component,
+            frame_a: Bump::new(),
+            frame_b: Bump::new(),
+            is_frame_a: true,
         }
     }
 
-    pub fn current() -> Self {
-        lazy_static! {
-            static ref RUNTIME: Runtime = Runtime::new();
-        }
-        RUNTIME.clone()
+    pub fn view<'a>(&mut self)
+    where
+        V: View<'a> + 'a,
+    {
+        let bump = if self.is_frame_a {
+            self.is_frame_a = false;
+            &self.frame_a
+        } else {
+            self.is_frame_a = true;
+            &self.frame_b
+        };
+        let bump: &'a Bump = unsafe { mem::transmute(bump) };
+        
+        let view = (self.component)(bump);       
+        bump.alloc(view).build();
+    }
+    
+}
+
+impl<'a> View<'a> for &'a str {
+    type Element = ();
+
+    fn build(&'a self) -> Self::Element {
+        dbg!(self);
+    }
+
+    fn rebuild(&'a self, element: &mut Self::Element) {
+        dbg!(self);
     }
 }
