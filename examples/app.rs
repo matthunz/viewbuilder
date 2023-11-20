d399b1180ecf75573f9faf43b9f2c30ccae2b7b8
Matt Hunzinger <matthunz2@gmail.com>
2023-11-20 00:19:13 -0500

    Impl message passing

--- examples/app.rs
+++ examples/app.rs
@@ -1,11 +1,21 @@
 use bumpalo::Bump;
 use viewbuilder::{Tree, View};
 
-fn app(bump: &Bump) -> impl View {
-    &**bump.alloc("")
+enum Message {
+    Increment,
+    Decrement,
+}
+
+fn app<'a>(bump: &'a Bump, count: &mut i32) -> impl View<'a, Message> {
+    &**bump.alloc(count.to_string())
 }
 
 fn main() {
-    let mut tree = Tree::new(app);
+    let mut tree = Tree::new(0, app, |count: &mut i32, msg| match msg {
+        Message::Increment => *count += 1,
+        Message::Decrement => *count -= 1,
+    });
+    tree.view();
+    tree.handle(Message::Increment);
     tree.view();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,40 +1,44 @@
 use bumpalo::Bump;
 use std::mem;
 
-pub trait View<'a> {
+pub trait View<'a, M> {
     type Element;
 
-    fn build(&'a self) -> Self::Element;
+    fn build(&'a mut self) -> Self::Element;
 
-    fn rebuild(&'a self, element: &mut Self::Element);
+    fn rebuild(&'a mut self, element: &mut Self::Element);
+
+    fn handle(&'a mut self, msg: M);
 }
 
-pub struct Tree<V> {
-    component: fn(&Bump) -> V,
+pub struct Tree<V, S, F> {
+    component: fn(&Bump, &mut S) -> V,
+    state: S,
+    handler: F,
     frame_a: Bump,
     frame_b: Bump,
     is_frame_a: bool,
 }
 
-impl<V> Tree<V> {
-    pub fn new<'a>(component: fn(&'a Bump) -> V) -> Self
+impl<V, S, F> Tree<V, S, F> {
+    pub fn new<'a, M>(state: S, component: fn(&'a Bump, &mut S) -> V, handler: F) -> Self
     where
-        V: View<'a> + 'a,
+        V: View<'a, M> + 'a,
     {
-        let component = unsafe {
-             mem::transmute(component)
-        };
+        let component = unsafe { mem::transmute(component) };
         Self {
             component,
+            handler,
+            state,
             frame_a: Bump::new(),
             frame_b: Bump::new(),
             is_frame_a: true,
         }
     }
 
-    pub fn view<'a>(&mut self)
+    pub fn view<'a, M>(&mut self)
     where
-        V: View<'a> + 'a,
+        V: View<'a, M> + 'a,
     {
         let bump = if self.is_frame_a {
             self.is_frame_a = false;
@@ -44,21 +48,30 @@ impl<V> Tree<V> {
             &self.frame_b
         };
         let bump: &'a Bump = unsafe { mem::transmute(bump) };
-        
-        let view = (self.component)(bump);       
+
+        let view = (self.component)(bump, &mut self.state);
         bump.alloc(view).build();
     }
-    
+
+    pub fn handle<'a, M>(&mut self, msg: M)
+    where
+        V: View<'a, M> + 'a,
+        F: FnMut(&mut S, M),
+    {
+        (self.handler)(&mut self.state, msg);
+    }
 }
 
-impl<'a> View<'a> for &'a str {
+impl<'a, M> View<'a, M> for &'a str {
     type Element = ();
 
-    fn build(&'a self) -> Self::Element {
+    fn build(&'a mut self) -> Self::Element {
         dbg!(self);
     }
 
-    fn rebuild(&'a self, element: &mut Self::Element) {
+    fn rebuild(&'a mut self, _element: &mut Self::Element) {
         dbg!(self);
     }
+
+    fn handle(&'a mut self, _msg: M) {}
 }
