39a92f08968fb1d079f55b1127b6f03c188a0bf7
Matt Hunzinger <matthunz2@gmail.com>
2023-11-21 18:02:40 -0500

    Create UI struct

--- examples/app.rs
+++ examples/app.rs
@@ -1,11 +1,15 @@
-use viewbuilder::{Text, Tree};
+use viewbuilder::{LocalTree, Text, UserInterface};
 
 fn main() {
-    let mut tree = Tree::default();
+    let mut ui = UserInterface::new();
 
-    let mut sub_tree = Tree::default();
-    let text = sub_tree.insert(Text::new("A"));
+    let sub_tree = ui.insert(LocalTree::builder());
+    let text = sub_tree.get_mut(&mut ui).insert(Text::new("A"));
+    dbg!(text.get(sub_tree.get_mut(&mut ui)).content());
 
-    let sub_tree_ref = tree.insert(sub_tree);
-    dbg!(text.get(&sub_tree_ref.get(&tree)).content());
+    let tree = ui.insert(LocalTree::builder());
+    tree.get_mut(&mut ui).insert(sub_tree);
+
+    Text::set_content(text, sub_tree.get_mut(&mut ui), "B");
+    dbg!(text.get(sub_tree.get_mut(&mut ui)).content());
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
-use slotmap::{DefaultKey, SlotMap, SparseSecondaryMap};
+use slotmap::{new_key_type, DefaultKey, SlotMap, SparseSecondaryMap};
 use std::{any::Any, borrow::Cow, marker::PhantomData};
+use tokio::sync::mpsc::{self, UnboundedReceiver, UnboundedSender};
 
 pub trait Element {
     type Message;
@@ -46,28 +47,29 @@ impl<E> Clone for ElementRef<E> {
 impl<E> Copy for ElementRef<E> {}
 
 impl<E> ElementRef<E> {
-    pub fn get(self, tree: &Tree) -> &E
+    pub fn get(self, tree: &LocalTree) -> &E
     where
         E: 'static,
     {
         tree.elements[self.key].as_any().downcast_ref().unwrap()
     }
 
-    pub fn get_mut(self, tree: &mut Tree) -> &mut E
+    pub fn get_mut(self, tree: &mut LocalTree) -> &mut E
     where
         E: 'static,
     {
         tree.elements[self.key].as_any_mut().downcast_mut().unwrap()
     }
 
-    pub fn send(self, tree: &mut Tree, msg: E::Message)
+    pub fn send(self, tree: &mut LocalTree, msg: E::Message)
     where
         E: Element + 'static,
     {
-        tree.elements[self.key].handle_any(Box::new(msg))
+        tree.elements[self.key].handle_any(Box::new(msg));
+        tree.tx.send(tree.key).unwrap();
     }
 
-    pub fn push_child(self, tree: &mut Tree, key: DefaultKey) {
+    pub fn push_child(self, tree: &mut LocalTree, key: DefaultKey) {
         if let Some(children) = tree.children.get_mut(self.key) {
             children.push(key);
         } else {
@@ -89,14 +91,19 @@ impl<E> ElementRef<E> {
     }
 }
 
-#[derive(Default)]
-pub struct Tree {
+pub struct LocalTree {
+    key: TreeKey,
+    tx: UnboundedSender<TreeKey>,
     elements: SlotMap<DefaultKey, Box<dyn AnyElement>>,
     children: SparseSecondaryMap<DefaultKey, Vec<DefaultKey>>,
     parents: SparseSecondaryMap<DefaultKey, DefaultKey>,
 }
 
-impl Tree {
+impl LocalTree {
+    pub fn builder() -> LocalTreeBuilder {
+        LocalTreeBuilder {}
+    }
+
     pub fn insert<E: Element + 'static>(&mut self, element: E) -> ElementRef<E> {
         let key = self.elements.insert(Box::new(element));
 
@@ -107,10 +114,18 @@ impl Tree {
     }
 }
 
-impl Element for Tree {
-    type Message = ();
+pub enum TreeMessage {
+    Handle { key: DefaultKey, msg: Box<dyn Any> },
+}
 
-    fn handle(&mut self, msg: Self::Message) {}
+impl Element for LocalTree {
+    type Message = TreeMessage;
+
+    fn handle(&mut self, msg: Self::Message) {
+        match msg {
+            TreeMessage::Handle { key, msg } => self.elements[key].handle_any(msg),
+        }
+    }
 }
 
 pub enum TextMessage {
@@ -134,7 +149,7 @@ impl Text {
 
     pub fn set_content(
         text: ElementRef<Self>,
-        tree: &mut Tree,
+        tree: &mut LocalTree,
         content: impl Into<Cow<'static, str>>,
     ) {
         text.send(tree, TextMessage::SetContent(content.into()))
@@ -149,6 +164,84 @@ impl Element for Text {
             TextMessage::SetContent(content) => {
                 self.content = content;
             }
+        }
+    }
+}
+
+new_key_type! {
+    pub struct TreeKey;
+}
+
+impl<T> Element for TreeRef<T> {
+    type Message = ();
+
+    fn handle(&mut self, msg: Self::Message) {}
+}
+
+pub trait TreeBuilder {
+    type Tree: 'static;
+
+    fn insert_with_key(self, key: TreeKey, tx: UnboundedSender<TreeKey>) -> Self::Tree;
+}
+
+pub struct LocalTreeBuilder {}
+
+impl TreeBuilder for LocalTreeBuilder {
+    type Tree = LocalTree;
+
+    fn insert_with_key(self, key: TreeKey, tx: UnboundedSender<TreeKey>) -> Self::Tree {
+        LocalTree {
+            key,
+            tx,
+            elements: Default::default(),
+            children: Default::default(),
+            parents: Default::default(),
+        }
+    }
+}
+
+pub struct TreeRef<T> {
+    key: TreeKey,
+    _marker: PhantomData<T>,
+}
+
+impl<T> Clone for TreeRef<T> {
+    fn clone(&self) -> Self {
+        *self
+    }
+}
+
+impl<T> Copy for TreeRef<T> {}
+
+impl<T: 'static> TreeRef<T> {
+    pub fn get_mut(self, ui: &mut UserInterface) -> &mut T {
+        ui.trees[self.key].downcast_mut().unwrap()
+    }
+}
+
+pub struct UserInterface {
+    trees: SlotMap<TreeKey, Box<dyn Any>>,
+    tx: UnboundedSender<TreeKey>,
+    rx: UnboundedReceiver<TreeKey>,
+}
+
+impl UserInterface {
+    pub fn new() -> Self {
+        let (tx, rx) = mpsc::unbounded_channel();
+        Self {
+            trees: SlotMap::default(),
+            tx,
+            rx,
+        }
+    }
+
+    pub fn insert<T: 'static>(&mut self, tree_builder: impl TreeBuilder<Tree = T>) -> TreeRef<T> {
+        let key = self
+            .trees
+            .insert_with_key(|key| Box::new(tree_builder.insert_with_key(key, self.tx.clone())));
+        TreeRef {
+            key,
+            _marker: PhantomData,
         }
     }
 }
