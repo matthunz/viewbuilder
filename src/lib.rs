a585039960196ed42a1dc1021ffd3c14bd2f0fc7
Matt Hunzinger <matthunz2@gmail.com>
2023-12-15 05:57:13 -0500

    Publish v0.8.0

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,23 @@
-#[cfg(feature = "EventLoop")]
-pub mod event_loop;
-#[cfg(feature = "EventLoop")]
-pub use event_loop::EventLoop;
+#![cfg_attr(docsrs, feature(doc_cfg))]
 
-#[cfg(feature = "Window")]
-pub mod window;
-#[cfg(feature = "Window")]
-pub use window::Window;
+macro_rules! cfg_flag {
+    ($flag:tt; $($i:item)*) => {
+        $(
+            #[cfg(feature = $flag)]
+            #[cfg_attr(docsrs, doc(cfg(feature = $flag)))]
+            $i
+        )*
+    };
+}
+
+cfg_flag!(
+    "EventLoop";
+    pub mod event_loop;
+    pub use event_loop::EventLoop;
+);
+
+cfg_flag!(
+    "Window";
+    pub mod window;
+    pub use window::Window;
+);
--- src/window.rs
+++ src/window.rs
@@ -5,27 +5,23 @@ use crate::{
 use concoct::{Context, Handle, Object, Signal};
 use winit::window::{Window as RawWindow, WindowBuilder};
 
-enum WindowState<E: 'static> {
+enum WindowState {
     Builder(WindowBuilder),
-    Window {
-        window: RawWindow,
-        event_loop: Handle<EventLoop<E>>,
-    },
+    Window { window: RawWindow },
 }
 
-
-pub struct Window<E: 'static> {
-    state: Option<WindowState<E>>,
+pub struct Window {
+    state: Option<WindowState>,
 }
 
-impl<E> Window<E> {
+impl Window {
     pub fn new() -> Self {
         Self {
             state: Some(WindowState::Builder(WindowBuilder::new())),
         }
     }
 
-    pub fn insert(cx: &mut Context<Self>, event_loop: &Handle<EventLoop<E>>)
+    pub fn insert<E>(cx: &mut Context<Self>, event_loop: &Handle<EventLoop<E>>)
     where
         E: Clone,
     {
@@ -37,26 +33,48 @@ impl<E> Window<E> {
                     EventLoopTarget::EventLoop(event_loop) => builder.build(event_loop).unwrap(),
                     EventLoopTarget::WindowTarget(event_loop) => builder.build(event_loop).unwrap(),
                 };
-                cx.state = Some(WindowState::Window {
-                    window,
-                    event_loop: event_loop.clone(),
-                });
+                cx.state = Some(WindowState::Window { window });
             }
             WindowState::Window { .. } => todo!(),
         }
     }
 
-    pub fn handle(cx: &mut Context<Self>, event: Event<E>) {
-        let window = match cx.state.as_ref().unwrap() {
-            WindowState::Window {
-                window,
-                event_loop: _,
-            } => window,
+    /// Get a reference to the raw `winit` window.
+    pub fn raw(&self) -> &RawWindow {
+        match self.state.as_ref().unwrap() {
+            WindowState::Window { window } => window,
             _ => todo!(),
-        };
+        }
+    }
+
+    /// Gets the window's current visibility state.
+    ///
+    /// `None` means it couldn't be determined, so it is not recommended to use this to drive your rendering backend.
+    ///
+    /// ## Platform-specific
+    ///
+    /// - **X11:** Not implemented.
+    /// - **Wayland / iOS / Android / Web:** Unsupported.
+    pub fn is_visible(&self) -> Option<bool> {
+        self.raw().is_visible()
+    }
+
+    /// Modifies the window's visibility.
+    ///
+    /// If `false`, this will hide the window. If `true`, this will show the window.
+    ///
+    /// ## Platform-specific
+    ///
+    /// - **Android / Wayland / Web:** Unsupported.
+    /// - **iOS:** Can only be called on the main thread.
+    pub fn set_visible(&self, visible: bool) {
+        self.raw().set_visible(visible)
+    }
+
+    pub fn handle<E>(cx: &mut Context<Self>, event: Event<E>) {
         match event {
             Event::Window { window_id, event } => {
-                if window_id == window.id() {
+                if window_id == cx.raw().id() {
                     cx.emit(event);
                 }
             }
@@ -65,6 +83,6 @@ impl<E> Window<E> {
     }
 }
 
-impl<E> Object for Window<E> {}
+impl Object for Window {}
 
-impl<E> Signal<WindowEvent> for Window<E> {}
+impl Signal<WindowEvent> for Window {}
