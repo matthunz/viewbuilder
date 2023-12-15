dda72223f3856dc3128957f43f10a71635da0323
Matt Hunzinger <matthunz2@gmail.com>
2023-12-15 04:17:05 -0500

    Create new Window object

--- examples/window.rs
+++ examples/window.rs
@@ -1,7 +1,15 @@
 use concoct::Object;
-use viewbuilder::window::EventLoop;
+use viewbuilder::window::{EventLoop, Window};
 
 fn main() {
     let event_loop = EventLoop::<()>::new().start();
+
+    let a = Window::new().start();
+    Window::insert(&mut a.cx(), &event_loop);
+
+    a.listen(|msg| {
+        dbg!(msg);
+    });
+
     EventLoop::run(event_loop);
 }
--- src/window.rs
+++ src/window.rs
@@ -1,60 +1,157 @@
-use concoct::{Handle, Object, Signal};
-use winit::event::{
-    DeviceEvent, DeviceId, Event as RawEvent, StartCause, WindowEvent as RawWindowEvent,
+use concoct::{Context, Handle, Object, Signal};
+use std::mem;
+use winit::{
+    dpi::PhysicalSize,
+    event::{DeviceEvent, DeviceId, Event as RawEvent, StartCause, WindowEvent as RawWindowEvent},
+    event_loop::EventLoopWindowTarget,
+    window::{Window as RawWindow, WindowBuilder, WindowId},
 };
 
-pub enum WindowEvent {}
+enum WindowState<E: 'static> {
+    Builder(WindowBuilder),
+    Window {
+        window: RawWindow,
+        event_loop: Handle<EventLoop<E>>,
+    },
+}
 
+pub struct Window<E: 'static> {
+    state: Option<WindowState<E>>,
+}
+
+impl<E> Window<E> {
+    pub fn new() -> Self {
+        Self {
+            state: Some(WindowState::Builder(WindowBuilder::new())),
+        }
+    }
+
+    pub fn insert(cx: &mut Context<Self>, event_loop: &Handle<EventLoop<E>>)
+    where
+        E: Clone,
+    {
+        event_loop.bind(&cx.handle(), Self::handle);
+
+        match cx.state.take().unwrap() {
+            WindowState::Builder(builder) => {
+                let window = match event_loop.borrow_mut().raw.as_mut().unwrap() {
+                    EventLoopTarget::EventLoop(event_loop) => builder.build(event_loop).unwrap(),
+                    EventLoopTarget::WindowTarget(event_loop) => builder.build(event_loop).unwrap(),
+                };
+                cx.state = Some(WindowState::Window {
+                    window,
+                    event_loop: event_loop.clone(),
+                });
+            }
+            WindowState::Window { .. } => todo!(),
+        }
+    }
+
+    pub fn handle(cx: &mut Context<Self>, event: Event<E>) {
+        let window = match cx.state.as_ref().unwrap() {
+            WindowState::Window { window, event_loop } => window,
+            _ => todo!(),
+        };
+        match event {
+            Event::Window { window_id, event } => {
+                if window_id == window.id() {
+                    cx.emit(event);
+                }
+            }
+            _ => {}
+        }
+    }
+}
+
+impl<E> Object for Window<E> {}
+
+impl<E> Signal<WindowEvent> for Window<E> {}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub enum WindowEvent {
+    Resized(PhysicalSize<u32>),
+    Focused(bool),
+    Occluded(bool),
+    Destroyed,
+}
+
+#[derive(Clone)]
 pub enum Event<E> {
     Custom(E),
+    Window {
+        window_id: WindowId,
+        event: WindowEvent,
+    },
     NewEvents(StartCause),
     Resumed,
     DeviceEvent {
         device_id: DeviceId,
         event: DeviceEvent,
     },
+    RedrawRequested(WindowId),
     MainEventsCleared,
     RedrawEventsCleared,
 }
 
+enum EventLoopTarget<T: 'static> {
+    EventLoop(winit::event_loop::EventLoop<T>),
+    WindowTarget(&'static EventLoopWindowTarget<T>),
+}
+
 pub struct EventLoop<E: 'static> {
-    raw: Option<winit::event_loop::EventLoop<E>>,
+    raw: Option<EventLoopTarget<E>>,
 }
 
 impl<E: 'static> EventLoop<E> {
     pub fn new() -> Self {
         Self {
-            raw: Some(winit::event_loop::EventLoopBuilder::with_user_event().build()),
+            raw: Some(EventLoopTarget::EventLoop(
+                winit::event_loop::EventLoopBuilder::with_user_event().build(),
+            )),
         }
     }
 
     pub fn run(handle: Handle<Self>) {
         let mut me = handle.borrow_mut();
-        let raw = me.raw.take().unwrap();
+        let raw = if let EventLoopTarget::EventLoop(raw) = me.raw.take().unwrap() {
+            raw
+        } else {
+            unimplemented!()
+        };
         drop(me);
 
-        raw.run(move |event, _, _| {
+        raw.run(move |event, event_loop, _| {
+            handle.borrow_mut().raw = Some(EventLoopTarget::WindowTarget(unsafe {
+                mem::transmute(event_loop.clone())
+            }));
+
             let event = match event {
                 RawEvent::UserEvent(custom) => Event::Custom(custom),
                 RawEvent::NewEvents(start_cause) => Event::NewEvents(start_cause),
-                RawEvent::WindowEvent {
-                    window_id: _,
-                    event,
-                } => match event {
+                RawEvent::WindowEvent { window_id, event } => match event {
                     RawWindowEvent::AxisMotion {
                         device_id: _,
                         axis: _,
                         value: _,
                     } => todo!(),
-                    RawWindowEvent::Resized(_) => todo!(),
+                    RawWindowEvent::Resized(size) => Event::Window {
+                        window_id,
+                        event: WindowEvent::Resized(size),
+                    },
                     RawWindowEvent::Moved(_) => todo!(),
                     RawWindowEvent::CloseRequested => todo!(),
-                    RawWindowEvent::Destroyed => todo!(),
+                    RawWindowEvent::Destroyed => Event::Window {
+                        window_id,
+                        event: WindowEvent::Destroyed,
+                    },
                     RawWindowEvent::DroppedFile(_) => todo!(),
                     RawWindowEvent::HoveredFile(_) => todo!(),
                     RawWindowEvent::HoveredFileCancelled => todo!(),
                     RawWindowEvent::ReceivedCharacter(_) => todo!(),
-                    RawWindowEvent::Focused(_) => todo!(),
+                    RawWindowEvent::Focused(focused) => Event::Window {
+                        window_id,
+                        event: WindowEvent::Focused(focused),
+                    },
                     RawWindowEvent::KeyboardInput {
                         device_id: _,
                         input: _,
@@ -103,7 +200,10 @@ impl<E: 'static> EventLoop<E> {
                         new_inner_size: _,
                     } => todo!(),
                     RawWindowEvent::ThemeChanged(_) => todo!(),
-                    RawWindowEvent::Occluded(_) => todo!(),
+                    RawWindowEvent::Occluded(occluded) => Event::Window {
+                        window_id,
+                        event: WindowEvent::Occluded(occluded),
+                    },
                 },
                 RawEvent::DeviceEvent { device_id, event } => {
                     Event::DeviceEvent { device_id, event }
@@ -111,12 +211,13 @@ impl<E: 'static> EventLoop<E> {
                 RawEvent::Suspended => todo!(),
                 RawEvent::Resumed => Event::Resumed,
                 RawEvent::MainEventsCleared => Event::MainEventsCleared,
-                RawEvent::RedrawRequested(_) => todo!(),
+                RawEvent::RedrawRequested(window_id) => Event::RedrawRequested(window_id),
                 RawEvent::RedrawEventsCleared => Event::RedrawEventsCleared,
                 RawEvent::LoopDestroyed => todo!(),
             };
 
-            handle.cx().emit(event)
+            handle.cx().emit(event);
+            handle.borrow_mut().raw = None;
         });
     }
 }
