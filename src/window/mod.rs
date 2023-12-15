004025febbb2aa49566f93857548dec2b6781448
Matt Hunzinger <matthunz2@gmail.com>
2023-12-15 07:16:56 -0500

    Fix docs

--- src/window/mod.rs
+++ src/window/mod.rs
@@ -18,14 +18,24 @@ enum WindowState {
 /// use concoct::{Context, Object};
 /// use viewbuilder::{event_loop::WindowEvent, EventLoop, Window};
 /// 
+/// struct App;
+/// 
+/// impl App {
+///     pub fn event(_cx: &mut Context<Self>, event: WindowEvent) {
+///         dbg!(event);
+///     }
+/// }
+/// 
+/// impl Object for App {}
+/// 
 /// let event_loop = EventLoop::<()>::new().start();
-///
+/// 
 /// let window = Window::new().start();
 /// Window::insert(&mut window.cx(), &event_loop);
-///
+/// 
 /// let app = App.start();
 /// window.bind(&app, App::event);
-///
+/// 
 /// EventLoop::run(event_loop);
 /// ```
 pub struct Window {
